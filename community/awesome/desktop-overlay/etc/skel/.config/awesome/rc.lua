38407c5bc96da6188e65774192a58282f519ff02
megvadulthangya <megvadulthangya@gmail.com>
2025-10-23 14:51:24 +0200

    add tilix to floating clients in rc.lua

--- community/awesome/desktop-overlay/etc/skel/.config/awesome/rc.lua
+++ community/awesome/desktop-overlay/etc/skel/.config/awesome/rc.lua
@@ -814,6 +814,7 @@ awful.rules.rules = {
           "DTA",  -- Firefox addon DownThemAll.
           "copyq",  -- Includes session name in class.
           "pinentry",
+          "tilix",
         },
         class = {
           "Arandr",
